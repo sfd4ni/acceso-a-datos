0fbc513f0f6c6ab6af33caa076bc79eaf4b5fe02
Daniel Barroso Rocío <danielbarrosorocio@gmail.com>
2021-10-20 20:18:22 +0100

    Post de foro irc

--- Matriculas/src/main/java/es/iespuertodelacruz/jc/matriculas/servlets/Principal.java
+++ Matriculas/src/main/java/es/iespuertodelacruz/jc/matriculas/servlets/Principal.java
@@ -8,35 +8,45 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import es.iespuertodelacruz.jc.matriculas.modelo.Mensaje;
+
 public class Principal extends HttpServlet {
 private static final long serialVersionUID = 1L;
        
     public Principal() {
         super();
     }
-protected void doGet(HttpServletRequest request, HttpServletResponse 
-response) throws ServletException, IOException {
-	/*String hola = request.getParameter("hola");
-	String adios = request.getParameter("adios");
-	String redireccion = "index.html";
-	if (hola != null && !hola.isEmpty()) {
-		redireccion = "hola.jsp";
-	}
-	if (adios != null && !adios.isEmpty()) {
-		redireccion = "adios.jsp";
-	}
-	request.getRequestDispatcher(redireccion).forward(request, response);*/
-	ArrayList<String> listaMensajes = (ArrayList<String>) 
-			request.getServletContext().getAttribute("mensajes");
-	if (listaMensajes == null) {
-		listaMensajes = new ArrayList<String>();
-		request.getServletContext().setAttribute("mensajes", listaMensajes);
-		for(int i = 0; i < 4; i++) {
-			listaMensajes.add("Iteracion " + i);
+    protected void doGet(HttpServletRequest request, HttpServletResponse 
+    		response) throws ServletException, IOException {
+		/*String hola = request.getParameter("hola");
+		String adios = request.getParameter("adios");
+		String redireccion = "index.html";
+		if (hola != null && !hola.isEmpty()) {
+			redireccion = "hola.jsp";
+		}
+		if (adios != null && !adios.isEmpty()) {
+			redireccion = "adios.jsp";
+		}
+		request.getRequestDispatcher(redireccion).forward(request, response);*/
+		ArrayList<Mensaje> listaMensajes = (ArrayList<Mensaje>) 
+				request.getServletContext().getAttribute("listaMensajes");
+		if (listaMensajes == null) {
+			listaMensajes = new ArrayList<Mensaje>();
+			request.getServletContext().setAttribute("listaMensajes", listaMensajes);
+			listaMensajes.add(new Mensaje("Juan Carlos", "Hola chicos, ¿qué tal?"));
+		}
+		request.getRequestDispatcher("vista.jsp").forward(request, response);
+		
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		ArrayList<Mensaje> listaMensajes = (ArrayList<Mensaje>) 
+				request.getServletContext().getAttribute("listaMensajes");
+		if (listaMensajes != null) {
+			listaMensajes.add(new Mensaje(request.getParameter("usuario"), request.getParameter("texto")));
 		}
 	}
-	request.getRequestDispatcher("vista.jsp").forward(request, response);
-	
-    }
 }
 
--- Matriculas/src/main/webapp/vista.jsp
+++ Matriculas/src/main/webapp/vista.jsp
@@ -3,15 +3,27 @@
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Bienvenido</title>
-</head>
-<body>
-el dato recibido es:
-
-<c:forEach items="${mensajes}" var="mensaje">
-	<c:out value="${mensaje}"/><br/>
-</c:forEach>
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>Bienvenido</title>
+		</head>
+	<body>
+		Foro:
+		
+		<c:forEach items="${listaMensajes}" var="mensaje">
+			<span><c:out value="${mensaje.getTexto()}"/>: <c:out value="${mensaje.getNombreUsuario()}"/></span><br/>
+		</c:forEach>
+		<form action="principal" method="POST"> 
+		
+			<label for="numero">Introduzca un mensaje:</label>
+			<br>
+			<label>Usuario: </label>
+			<input type="text" name="usuario" id="usuario"  />
+			<label> Mensaje: </label>
+			<input type="text" name="texto" id="texto"  />
+			<br>
+			<input type="submit" value="Enviar" />
+		                          
+		</form>	
+	</body>
 </html>
