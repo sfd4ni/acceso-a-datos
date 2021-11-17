7585298cc343c41e7fe5c45bb667b0efa08e0f1e
sfd4ni <danielbarrosorocio@gmail.com>
2021-11-17 19:22:25 +0000

    PruebaJpa funcionando

--- PruebaMavenJPAinicial/src/main/java/es/iespuertodelacruz/daniel/jpainicial/Main.java
+++ PruebaMavenJPAinicial/src/main/java/es/iespuertodelacruz/daniel/jpainicial/Main.java
@@ -1,9 +1,18 @@
 package es.iespuertodelacruz.daniel.jpainicial;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import es.iespuertodelacruz.daniel.jpainicial.entities.Alumno;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("PruebaMavenJPAinicial");
+		EntityManager em = emf.createEntityManager();
+		Alumno alu = em.find(Alumno.class, "12312312K");
+		System.out.println( alu.getNombre() + " " + alu.getApellidos());
 
 	}
 
