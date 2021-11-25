9b36daf502f9e2187f3f69b30de124ed24e7dc00
sfd4ni <danielbarrosorocio@gmail.com>
2021-11-25 15:25:20 +0000

    Inserts en desarrollo en monedas

--- monedaswebjpa-main/src/main/java/es/iespuertodelacruz/jc/monedaswebjpa/servlets/GestorMonedas.java
+++ monedaswebjpa-main/src/main/java/es/iespuertodelacruz/jc/monedaswebjpa/servlets/GestorMonedas.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import es.iespuertodelacruz.jc.monedaswebjpa.entities.Moneda;
 import es.iespuertodelacruz.jc.monedaswebjpa.repositories.MonedaRepository;
 
 /**
@@ -34,6 +35,42 @@ public class GestorMonedas extends HttpServlet {
 		String idStr = request.getParameter("id");
 		EntityManagerFactory emf =(EntityManagerFactory)request.getServletContext().getAttribute("emf");
 		MonedaRepository monedaR = new MonedaRepository(emf);
+		EntityManager em = emf.createEntityManager();
+		
+		Moneda moneda = new Moneda();
+		moneda.setNombre("Lira32");
+		moneda.setPais("Turquía");
+		em.getTransaction().begin();
+		em.persist(moneda); // moneda vigilada → está en contexto
+		em.getTransaction().commit(); //moneda guardada en DDBB
+		
+		System.out.println("1: en base de datos: ");
+		mostrarMonedaEnDDBB(moneda.getIdmoneda(), emf);
+		em.close(); //moneda separada→ detached Cambios no persisten en DDBB
+		em = emf.createEntityManager(); // nueva conexión a la DDBB
+		em.getTransaction().begin(); //transact abierta para grabar en DDBB
+		moneda.setNombre("Lira34"); //la moneda detach... ¡¡ no se grabará !!
+		em.getTransaction().commit(); // da igual el commit.. no hay efecto
+		
+		System.out.println("2: en base de datos: ");
+		mostrarMonedaEnDDBB(moneda.getIdmoneda(), emf);
+		Moneda monedaVigilada = em.merge(moneda); //monedaVigilada  
+		//debemos trabajar con monedaVigilada no con moneda para cambios
+		//aunque en apariencia el system.out nos las muestra iguales
+		System.out.println("monedavigilada: " + monedaVigilada);
+		System.out.println("moneda: " + moneda);
+		monedaVigilada.setNombre("lira35"); //entity managed
+		moneda.setNombre("lira36");  //entity detached
+		
+		System.out.println("3: en base de datos antes de transact: ");
+		mostrarMonedaEnDDBB(moneda.getIdmoneda(), emf);
+		em.getTransaction().begin();
+		em.getTransaction().commit(); //ahora sí cambia la DDBB
+		
+		System.out.println("4: en base de datos: después de transact");
+		mostrarMonedaEnDDBB(moneda.getIdmoneda(), emf);
+		em.close();
+		
 		String redirect = "users/monedas.jsp";
 		if (idStr != null) {
 			Integer idMoneda = null;
@@ -58,5 +95,15 @@ public class GestorMonedas extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+	
+	public static void mostrarMonedaEnDDBB(
+			Integer idmoneda,
+			EntityManagerFactory emf
+			     ) {
+			EntityManager em = emf.createEntityManager();
+			Moneda moneda = em.find(Moneda.class, idmoneda);
+			System.out.println(moneda.toString() + moneda.getIdmoneda());
+			em.close();
+			}
 
 }
