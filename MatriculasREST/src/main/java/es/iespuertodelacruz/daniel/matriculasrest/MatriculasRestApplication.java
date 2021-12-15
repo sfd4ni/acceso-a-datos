1241f0a00b136cbb5117d45e50b117f3cf24fe85
sfd4ni <danielbarrosorocio@gmail.com>
2021-12-15 17:54:20 +0000

    Comenzamos proyecto MatriculasREST

---
+++ MatriculasREST/src/main/java/es/iespuertodelacruz/daniel/matriculasrest/MatriculasRestApplication.java
@@ -0,0 +1,13 @@
+package es.iespuertodelacruz.daniel.matriculasrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculasRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculasRestApplication.class, args);
+	}
+
+}
---
+++ MatriculasREST/src/test/java/es/iespuertodelacruz/daniel/matriculasrest/MatriculasRestApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iespuertodelacruz.daniel.matriculasrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculasRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
